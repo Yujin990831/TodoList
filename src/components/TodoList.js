db70e323ac4eaf5c4fe6df72c1a698b90dbe49e3
Yujin <cyjnos7572@naver.com>
2021-11-07 22:29:57 +0900

    TodoItem 컴포넌트 만들기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import Todoitem from './TodoItem';
 
 const TodoListBlock = styled.div`
     flex: 1;
     padding : 20px 32px;
     padding-bottom : 48px;
     overflow-y : auto;
-    background : gray;
-`
+    
+`;
 
 function TodoList() {
     return(
         <TodoListBlock>
-            TodoList
+           <Todoitem text = "프로젝트 생성하기" done={true} />
+           <Todoitem text = "컴포넌트 스타일링 하기" done={true} />
+           <Todoitem text = "Context 만들기" done={false} />
+           <Todoitem text = "기능 구현하기" done={false} />
         </TodoListBlock>
     );
 }
