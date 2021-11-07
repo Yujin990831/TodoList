4c88b5b39d5c6baf25b7c1be347ccde5db389b00
Yujin <cyjnos7572@naver.com>
2021-11-07 23:39:34 +0900

    TodoList 컴포넌트 기능 구현하기

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import Todoitem from './TodoItem';
+import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
 
 const TodoListBlock = styled.div`
     flex: 1;
@@ -11,12 +12,17 @@ const TodoListBlock = styled.div`
 `;
 
 function TodoList() {
+    const todos = useTodoState();
     return(
         <TodoListBlock>
-           <Todoitem text = "프로젝트 생성하기" done={true} />
-           <Todoitem text = "컴포넌트 스타일링 하기" done={true} />
-           <Todoitem text = "Context 만들기" done={false} />
-           <Todoitem text = "기능 구현하기" done={false} />
+           {todos.map(
+               todo => <TodoItem
+               key = {todo.id}
+               id = {todo.id}
+               text = {todo.text}
+               done = {todo.done}
+               />
+           )}
         </TodoListBlock>
     );
 }
